99f2c0ad5ef985151949727cfe0ca8ba7d0ed8b9
Zach <40329049+zhudso@users.noreply.github.com>
2020-09-30 10:02:23 -0500

    Add files via upload

---
+++ MockUp.ps1
@@ -0,0 +1,96 @@
+
+#---------------------------------------------------------[Pre Reqs]---------------------------------------------------------
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[void] [System.Windows.Forms.Application]::EnableVisualStyles()
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+
+#---------------------------------------------------------[GUI]---------------------------------------------------------
+$basicForm = New-Object System.Windows.Forms.Form
+
+$givenName                     = New-Object system.Windows.Forms.Label
+$givenName.text                = "First Name"
+$givenName.AutoSize            = $true
+$givenName.width               = 150
+$givenName.height              = 50
+$givenName.location            = New-Object System.Drawing.Point(20,5)
+$givenName.Font                = 'Microsoft Sans Serif,10'
+
+$givenNameTxtBox               = New-Object System.Windows.Forms.TextBox
+$givenNameTxtBox.Location      = '23,25'
+$givenNameTxtBox.Size          = '150,25'
+
+$Username                     = New-Object system.Windows.Forms.Label
+$Username.text                = "Username"
+$Username.AutoSize            = $true
+$Username.width               = 150
+$Username.height              = 50
+$Username.location            = New-Object System.Drawing.Point(20,60)
+$Username.Font                = 'Microsoft Sans Serif,10'
+
+$usernameTxtBox               = New-Object System.Windows.Forms.TextBox
+$usernameTxtBox.Location      = '23,80'
+$usernameTxtBox.Size          = '150,23'
+
+$Password                     = New-Object System.Windows.Forms.Label
+$Password.text                = "Password"
+$Password.AutoSize            = $true
+$Password.width               = 150
+$Password.height              = 50
+$Password.location            = New-Object System.Drawing.Point(20,110)
+$Password.Font                = 'Microsoft Sans Serif,10'
+
+$passwordTxtBox               = New-Object System.Windows.Forms.MaskedTextBox
+$passwordTxtBox.PasswordChar  = "*"
+$passwordTxtBox.Location      = '23,130'
+$passwordTxtBox.Size          = '150,23'
+
+$submitButton                 = New-Object System.Windows.Forms.Button
+$submitButton.Text            = 'Submit'
+$submitButton.Width           = 147
+$submitButton.Height          = 32
+$submitButton.Location        = New-Object System.Drawing.Point(20,220)
+$submitButton.Anchor          = [System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Left
+
+$buttonType                   = [System.Windows.MessageBoxButton]::YesNo
+$messageIcon                  = [System.Windows.MessageBoxImage]::Warning
+$messageBody                  = "Is the information provided is correct?"
+$messageTitle                 = "Confirm Submission"
+
+$basicForm.controls.AddRange(@($givenName, $givenNameTxtBox, $Username, $usernameTxtBox, $Password, $passwordTxtBox, $submitButton))
+
+#---------------------------------------------------------[Functions]---------------------------------------------------------
+
+function newUser {
+    $splat = @{
+        name              = $user.displayName
+        accountpassword   = $securePW
+        givenname         = $user.givenname
+        surname           = $user.Surname
+        Samaccountname    = $usernameTxtBox.Text
+        userprincipalname = $user.emailaddress
+        department        = $user.department 
+        Title             = $user.jobtitle 
+        displayname       = $user.displayname 
+        emailaddress      = $user.emailaddress
+        path              = $copiedOU
+        Enabled           = $true
+        verbose           = $true
+    }
+    Write-Host @splat -ErrorAction Stop
+    Write-Host $usernameTxtBox.Text $securePW.Text
+}
+
+#---------------------------------------------------------[Scripts]---------------------------------------------------------
+
+$submitButton.Add_Click({
+    $confirmationWindow = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+    if ($confirmationWindow -eq "Yes") {
+        $securePW = $passwordTxtBox.Text | ConvertTo-SecureString -AsPlainText -Force
+        newUser
+    }
+    else {
+        <# Do nothing, allows user to re-enter information #>
+    }
+})
+$basicForm.ShowDialog()
